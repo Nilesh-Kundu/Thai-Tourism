29531d0efc2ba20edcf345ee183319f2dc2ea4de
Nilesh-Kundu <56626534+Nilesh-Kundu@users.noreply.github.com>
2019-10-18 19:56:44 +0530

    Initial commit

---
+++ thaitourism_code.R
@@ -0,0 +1,53 @@
+library(forecast)
+setwd("C:\\Users\\ADMIN\\Desktop\\R Models\\Time Series")
+data<- read.csv("Afrofothers.csv")
+head(data)
+names(data)[c(1:2)]<- c("date","sales")
+
+
+## converting into timeseries data
+data<-ts(data[,2],start = c(2010,1),frequency = 12)
+
+## plotting the data to have a look at it
+plot(data)
+
+## Differencing the data to get rid off the trend
+plot(diff(data))
+
+
+## Log transforming to get rid off the uneven variance
+plot(log10(data))
+
+
+## Combining both log and differencing to get rid off trend and uneven variance
+plot(diff(log10(data)))
+
+
+## Augmented Dickey-Fuller (ADF) Test
+library(fUnitRoots)
+adfTest(data)
+adfTest(diff(data))
+
+
+
+## running ARIMA model
+require(forecast)
+ARIMAfit <- auto.arima(log10(data), approximation=TRUE,trace=TRUE)
+summary(ARIMAfit)
+
+
+
+## predicting the future values
+pred <- predict(ARIMAfit, n.ahead = 12)
+pred
+
+## showing values
+10^(pred$pred)
+pred <- 10^(pred$pred)
+write.csv(pred,"Afrofothers$result.csv")
+
+
+## plotting the forecasted values
+plot(forecast(ARIMAfit,h=12))
+
+write.csv(pred,"Sales$result.csv")
